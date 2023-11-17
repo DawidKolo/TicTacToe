e8c767b340fd64c5dc51f0746e8e43460182c634
Dawid Kolodziej <dawid.kolodziej1@precisely.com>
2023-11-17 13:38:44 +0100

    reformat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 from IPython.display import clear_output
+
 
 def player_markers():
     marker = ''
@@ -13,16 +13,18 @@ def player_markers():
         player_2 = 'O'
     else:
         player_2 = 'X'
-    return (player_1,player_2)
+    return (player_1, player_2)
+
 
 def display_board(board):
     clear_output()
-    print(board[1]+'|'+board[2]+'|'+board[3])
+    print(board[1] + '|' + board[2] + '|' + board[3])
     print('-+-+-')
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[7] + '|' + board[8] + '|' + board[9])
 
-board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+
+board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 player_markers()
 display_board(board)
