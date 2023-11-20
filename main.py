468f2ac63a39d41145b332df642b22367cb87345
Dawid Kolodziej <dawid.kolodziej1@precisely.com>
2023-11-20 12:42:58 +0100

    players move while loop correction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from IPython.display import clear_output
+
 # board list
 board = ['#', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 
 # choosing if player is X or O
 def player_markers():
@@ -17,51 +19,54 @@ def player_markers():
         player_2 = 'X'
     return (player_1, player_2)
 
+
 # displaying the game board
 def display_board(board):
     clear_output()
     print('')
     print('')
     print('   |   |   ')
-    print(' '+board[1]+' ' + '|'+' ' + board[2] + ' '+'|'+' ' + board[3])
+    print(' ' + board[1] + ' ' + '|' + ' ' + board[2] + ' ' + '|' + ' ' + board[3])
     print('   |   |   ')
     print('-----------')
     print('   |   |   ')
-    print(' '+board[4]+' ' + '|'+' ' + board[5] + ' '+'|'+' ' + board[6])
+    print(' ' + board[4] + ' ' + '|' + ' ' + board[5] + ' ' + '|' + ' ' + board[6])
     print('   |   |   ')
     print('-----------')
     print('   |   |   ')
-    print(' '+board[7]+' ' + '|'+' ' + board[8] + ' '+'|'+' ' + board[9])
+    print(' ' + board[7] + ' ' + '|' + ' ' + board[8] + ' ' + '|' + ' ' + board[9])
     print('   |   |   ')
     print('')
     print('')
+
 
 #
 def marker_placing(board, marker, position):
-
     board[position] = marker
-#taking players input
+
+
+# taking players input
 def players_move():
     position = 0
 
-    while position not in ['1','2','3','4','5','6','7','8','9'] or space_check(board, position):
-        position = input('Please select a position of your marker [1-9]: ')
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or space_check(board, position):
+        temp = input('Please select a position of your marker [1-9]: ')
+        position = int(temp)
+    return position
 
-    return int(position)
+
 # check if the space is available
 def space_check(board, position):
     return board[position] == 'O' or board[position] == 'X'
 
 
-
 #####################################################################
-#testing area
+# testing area
 
-#player_markers()
+# player_markers()
 
 marker_placing(board, 'O', 2)
 print(space_check(board, 2))
 display_board(board)
-#print(board)
-#players_move()
-
+# print(board)
+players_move()
