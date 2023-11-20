93003504ef52a404415b434ab278b1daa0ff88c4
Dawid Kolodziej <dawid.kolodziej1@precisely.com>
2023-11-20 14:07:24 +0100

    winner check

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from IPython.display import clear_output
 
 # board list
-board = ['#', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+board = ['#', 'X', 'X', 'X', '4', '5', '6', '7', '8', '9']
+
+#test_board = ['X']*10
 
 
 # choosing if player is X or O
@@ -59,14 +61,31 @@ def players_move():
 def space_check(board, position):
     return board[position] == 'O' or board[position] == 'X'
 
+def full_board(board):
+    for i in range(1, 10):
+        if not space_check(board, i):
+            return False
+    return True
+
+def winner_chck(board, mark):
+    return ((board[1] == mark and board[2] == mark and board[3] == mark) or
+            (board[4] == mark and board[5] == mark and board[6] == mark) or
+            (board[7] == mark and board[8] == mark and board[9] == mark) or
+            (board[1] == mark and board[4] == mark and board[7] == mark) or
+            (board[2] == mark and board[5] == mark and board[8] == mark) or
+            (board[3] == mark and board[6] == mark and board[9] == mark) or
+            (board[1] == mark and board[5] == mark and board[9] == mark) or
+            (board[3] == mark and board[5] == mark and board[7] == mark))
 
 #####################################################################
 # testing area
 
 # player_markers()
 
-marker_placing(board, 'O', 2)
-print(space_check(board, 2))
+#marker_placing(board, 'O', 2)
+#print(space_check(board, 2))
 display_board(board)
 # print(board)
-players_move()
+#players_move()
+print(board)
+print(winner_chck(board, 'X'))
